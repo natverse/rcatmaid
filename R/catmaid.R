d0765f23290791573ed6622f00a9fb3b3cdb2d29
Gregory Jefferis <jefferis@gmail.com>
2014-10-23 17:54:48 +0100

    verified and documented version of catmaid login
    * works fine for my own login

--- R/catmaid.R
+++ R/catmaid.R
@@ -7,4 +7,5 @@
 #' @aliases catmaid
 #' @docType package
 #' @keywords package
+#' @import httr
 NULL
--- R/catmaid_connection.R
+++ R/catmaid_connection.R
@@ -1,16 +1,23 @@
-catmaid_connection<-function(server=getOption("catmaid.server"), 
-                             authname=getOption("catmaid.authname"), 
-                             authpassword=getOption("catmaid.authpassword"), 
-                             username=getOption("catmaid.username"),
-                             password=getOption("catmaid.password")) {
-  if(is.null(server) || !grepl("^https", server)) stop("Must provide a valid https server")
-  conn=list(server=server, authname=authname, authpassword=authpassword, username=username, password=password)
-  class(conn)="catmaid_connection"
-  conn
-}
-
-catmaid_login<-function(conn, ..., Force=FALSE){
-  if(missing(conn)) conn=catmaid_connection(...)
+#' Connect/authenticate to a CATMAID server, returning a connection object
+#' 
+#' @description \code{catmaid_login} allows you to login to a CATMAID server 
+#'   specified by a \code{catmaid_connection} object. If such an object is not 
+#'   specified, then one is created, by default using \code{options} of the form
+#'   "catmaid.*".
+#'   
+#'   The connection object returned by \code{catmaid_login} will then be used 
+#'   for future requests to the CATMAID server.
+#'   
+#' @details After successful login, the \code{catmaid_connection} will contain a
+#'   \code{cookie} field that includes a sessionid that is required for
+#'   subsequent GET/POST operations.
+#'   
+#' @param conn An optional \code{catmaid_connection} connection object.
+#' @param ... Additional arguemnts passed to catmaid_connection
+#' @param Force Whether to force a new login to the CATMAID server (default 
+#'   \code{FALSE})
+catmaid_login<-function(conn=NULL, ..., Force=FALSE){
+  if(is.null(conn)) conn=catmaid_connection(...)
   else if(!is.null(conn$authresponse) && !Force) {
     # Bail if we have already logged in (and don't want to login afresh)
     return(conn)
@@ -24,6 +31,23 @@ catmaid_login<-function(conn, ..., Force=FALSE){
            body=list(name=conn$username,pwd=conn$password),
            authenticate(conn$authname,conn$authpassword))
   }
-  conn$cookies=cookies(conn$authresponse)
+  conn$cookies=unlist(cookies(conn$authresponse))
   conn
 }
+
+#' @name catmaid_login
+#' @param server url of CATMAID server
+#' @param username,password Your CATMAID username and password.
+#' @param authname,authpassword The http basicauth username/password that
+#'   optionally secures the CATMAID server.
+catmaid_connection<-function(server=getOption("catmaid.server"), 
+                             username=getOption("catmaid.username"),
+                             password=getOption("catmaid.password"),
+                             authname=getOption("catmaid.authname"), 
+                             authpassword=getOption("catmaid.authpassword") ) {
+  if(is.null(server) || !grepl("^https", server)) stop("Must provide a valid https server")
+  conn=list(server=server, authname=authname, authpassword=authpassword, username=username, password=password)
+  class(conn)="catmaid_connection"
+  conn
+}
+
