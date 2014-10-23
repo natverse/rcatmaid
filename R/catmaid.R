5c2184b3e392e4da03659517f91149ea636f5579
Gregory Jefferis <jefferis@gmail.com>
2014-10-23 14:49:25 +0100

    package docs

---
+++ R/catmaid.R
@@ -0,0 +1,10 @@
+#' Programmatic access to the CATMAID API
+#' 
+#' See http://catmaid.org/.
+#' 
+#' Based on https://github.com/acardona/CATMAID/blob/master/scripts/remote/access.py
+#' @name catmaid-package
+#' @aliases catmaid
+#' @docType package
+#' @keywords package
+NULL
