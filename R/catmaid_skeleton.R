bd2b192fcff760475b4f94694d8ee39eddc48049
Gregory Jefferis <jefferis@gmail.com>
2014-10-23 20:36:40 +0100

    document and export catmaid_get_compact_skeleton

--- R/catmaid_skeleton.R
+++ R/catmaid_skeleton.R
@@ -1,6 +1,28 @@
-#' return the raw data for a CATMAID neuron skeleton
+#' return the raw data for a CATMAID neuronal skeleton
 #' 
-catmaid_get_compact_skeleton<-function(pid, skid, connectors = TRUE, tags = TRUE, conn=NULL, raw=FALSE, ...) {
+#' @param pid,skid project and skeleton ids
+#' @param conn the \code{\link{catmaid_connection}} object
+#' @param connectors Whether to fetch connector information
+#' @param tags Whether to fetch tag information
+#' @param raw Whether to return completely unprocessed data (when \code{TRUE}) 
+#'   or to convert the nodes and connectors lists into processed data.frames 
+#'   (when \code{FALSE}, the default)
+#' @param ... Additional arguments passed to the \code{\link{catmaid_GETJ}} 
+#'   function.
+#' @seealso \code{link{catmaid_GETJ}}
+#' @export
+#' @examples
+#' \dontrun{
+#' ## using an existing connection object
+#' conn=catmaid_login()
+#' skel=catmaid_get_compact_skeleton(1, 10418394, conn)
+#' # no connector (i.e. synapse) information
+#' skel=catmaid_get_compact_skeleton(1, 10418394, conn, connectors = FALSE)
+#' 
+#' ## using throwaway connection object
+#' skel=catmaid_get_compact_skeleton(1, 10418394)
+#' }
+catmaid_get_compact_skeleton<-function(pid, skid, conn=NULL, connectors = TRUE, tags = TRUE, raw=FALSE, ...) {
   path=file.path("", pid, skid, ifelse(connectors, 1L, 0L), ifelse(tags, 1L, 0L), "compact-skeleton")
   skel=catmaid_GETJ(path, conn=conn, ...)
   names(skel)=c("nodes", "connectors", "tags")
@@ -11,9 +33,10 @@ catmaid_get_compact_skeleton<-function(pid, skid, connectors = TRUE, tags = TRUE
                      cols=c("id", "parent_id", "user_id", "location.x",
                             "location.y", "location.z", "radius", "confidence"))
   
-  skel$connectors=list2df(skel$connectors, 
-                          cols=c("XXX", "connector_id", "prepost", "location.x",
-                                 "location.y", "location.z"))
+  if(length(skel$connectors))
+    skel$connectors=list2df(skel$connectors, 
+                            cols=c("XXX", "connector_id", "prepost", "location.x",
+                                   "location.y", "location.z"))
   skel
 }
 
