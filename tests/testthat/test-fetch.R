59d947406ff8870254dbb9a776438fdf61a97df2
Gregory Jefferis <jefferis@gmail.com>
2014-10-24 17:03:51 +0100

    tests for catmaid neuron queries

--- tests/testthat/test-fetch.R
+++ tests/testthat/test-fetch.R
@@ -9,3 +9,17 @@ test_that("get neuron names", {
                  structure(c("IPC10", "IPC1"), .Names = c("10418394", "4453485")))
   }
 })
+
+context("catmaid neuron queries")
+
+test_that("get neuron", {
+  if(!inherits(conn, 'try-error')){
+    expect_is(skel<-catmaid_get_compact_skeleton(pid=1, skid=10418394, conn=conn), 'list')
+  }
+})
+
+test_that("read.neron.catmaid", {
+  if(!inherits(conn, 'try-error')){
+    expect_is(skel<-read.neuron.catmaid(pid=1, skid=10418394, conn=conn), 'neuron')
+  }
+})
