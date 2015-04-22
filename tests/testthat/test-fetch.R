dbcd282d0600e207c1105a23c2fe2d55d0780839
Gregory Jefferis <jefferis@gmail.com>
2015-04-22 12:13:55 +0100

    add test for catmaid_get_neuronnames
    * this enforces https://github.com/jefferis/rcatmaid/issues/6

--- tests/testthat/test-fetch.R
+++ tests/testthat/test-fetch.R
@@ -23,3 +23,10 @@ test_that("read.neuron.catmaid", {
     expect_is(n<-read.neuron.catmaid(pid=1, skid=10418394, conn=conn), 'neuron')
   }
 })
+
+test_that("catmaid_get_neuronnames", {
+  if(!inherits(conn, 'try-error')){
+    baseline=c(`4453485`="IPC1", `10418394`="IPC10")
+    expect_equal(catmaid_get_neuronnames(skids=c(4453485,10418394)), baseline)
+  }
+})
