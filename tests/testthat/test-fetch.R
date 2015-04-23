41f3a4f277c8453273d5eb9d965a4f1ebc4ec17b
Gregory Jefferis <jefferis@gmail.com>
2015-04-23 09:40:10 +0100

    rationalise get_neuron_names tests
    * there were two almost identical - the second one ensured
      that the ordering of output was correct

--- tests/testthat/test-fetch.R
+++ tests/testthat/test-fetch.R
@@ -3,10 +3,10 @@ context("catmaid metadata queries")
 # nb will reuse cached connection made earlier
 conn=try(catmaid_login(), silent = TRUE)
 
-test_that("get neuron names", {
+test_that("catmaid_get_neuronnames", {
   if(!inherits(conn, 'try-error')){
-    expect_equal(catmaid_get_neuronnames(pid=1, skids=c(10418394,4453485)),
-                 structure(c("IPC10", "IPC1"), .Names = c("10418394", "4453485")))
+    baseline=c(`4453485`="IPC1", `10418394`="IPC10")
+    expect_equal(catmaid_get_neuronnames(skids=c(4453485,10418394)), baseline)
   }
 })
 
@@ -21,12 +21,5 @@ test_that("get neuron", {
 test_that("read.neuron.catmaid", {
   if(!inherits(conn, 'try-error')){
     expect_is(n<-read.neuron.catmaid(pid=1, skid=10418394, conn=conn), 'neuron')
-  }
-})
-
-test_that("catmaid_get_neuronnames", {
-  if(!inherits(conn, 'try-error')){
-    baseline=c(`4453485`="IPC1", `10418394`="IPC10")
-    expect_equal(catmaid_get_neuronnames(skids=c(4453485,10418394)), baseline)
   }
 })
