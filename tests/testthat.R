022f6bc8c7f42e97c23b6229cda55e41c96f2155
Gregory Jefferis <jefferis@gmail.com>
2015-04-27 10:09:59 +0100

    message explaining login required for tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,12 @@
 library(testthat)
 library(catmaid)
 
+try(conn<-catmaid:::catmaid_connection_getenv())
+if(inherits(conn,'try-error')) {
+  message("Full tests depend on having a valid connection to a catmaid server.\n",
+          "You must login in to catmaid (using catmaid_login() or by putting\n",
+          "login details in your .Rprofile) and then export the connection details\n",
+          "to environment variables using catmaid:::catmaid_connection_setenv().")
+}
+
 test_check("catmaid")
