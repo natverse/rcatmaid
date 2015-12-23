e54926d682e1a9be7f74b3338fd97d5ea19efc00
Gregory Jefferis <jefferis@gmail.com>
2015-12-23 14:20:47 +0000

    Add catmaid_get_annotations_for_skeletons
    * closes #38

---
+++ R/catmaid_annotations.R
@@ -0,0 +1,42 @@
+#' Get annotations for neurons from CATMAID
+#' 
+#' @inheritParams read.neuron.catmaid
+#' @return a data.frame containing the following columns \itemize{
+#'   
+#'   \item skid The skeleton id
+#'   
+#'   \item annotation The annotation string
+#'   
+#'   \item id The annotation id
+#'   
+#'   \item uid The user id for the annotation
+#'   
+#'   }
+#' @export
+#' @examples
+#' \dontrun{
+#' catmaid_get_annotations_for_skeletons(skids=c(10418394,4453485))
+#' catmaid_get_annotations_for_skeletons("name:ORN (left|right)")
+#' catmaid_get_annotations_for_skeletons("annotation:ORN PNs$")
+#' }
+#' @seealso \code{\link{catmaid_fetch}}, \code{\link{catmaid_skids}}
+catmaid_get_annotations_for_skeletons<-function(skids, pid=1, conn=NULL, ...) {
+  skids=catmaid_skids(skids, conn = conn)
+  post_data=list()
+  post_data[sprintf("skeleton_ids[%d]", seq_along(skids))]=as.list(skids)
+  path=sprintf("/%d/annotations/forskeletons", pid)
+  res=catmaid_fetch(path, body=post_data, include_headers = F, 
+                    simplifyVector = T, ...)
+  
+  # make a simple character vector with the annotation names
+  al=unlist(res$annotations)
+  aldf=data.frame(id=as.integer(names(al)), annotation=al)
+  
+  # rbind all the per skelenton data.frames into a single data.frame
+  skdf=do.call(rbind, res$skeletons)
+  # nb handle situation where neuron has no annotations
+  nrows=sapply(res$skeletons, function(x) {nr=nrow(x); if(is.null(nr)) 0 else nr})
+  skdf$skid=rep(as.integer(names(res$skeletons)), nrows)
+  skdf=merge(skdf, aldf, by='id')
+  skdf=skdf[c("skid", "annotation", "id", "uid")]
+}
