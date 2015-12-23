0dc444e9cd767d2133b200606fe4f03b5c6630a3
Gregory Jefferis <jefferis@gmail.com>
2015-12-23 15:04:09 +0000

    Add catmaid_set_annotations_for_skeletons

--- R/catmaid_annotations.R
+++ R/catmaid_annotations.R
@@ -1,7 +1,8 @@
-#' Get annotations for neurons from CATMAID
+#' Get or set annotations for neurons from CATMAID
 #' 
 #' @inheritParams read.neuron.catmaid
-#' @return a data.frame containing the following columns \itemize{
+#' @return For \code{catmaid_get_annotations_for_skeletons} a data.frame 
+#'   containing the following columns \itemize{
 #'   
 #'   \item skid The skeleton id
 #'   
@@ -12,6 +13,9 @@
 #'   \item uid The user id for the annotation
 #'   
 #'   }
+#'   
+#'   For \code{catmaid_set_annotations_for_skeletons} a list containing 
+#'   information about the annotations that have just been added.
 #' @export
 #' @examples
 #' \dontrun{
@@ -39,4 +43,27 @@ catmaid_get_annotations_for_skeletons<-function(skids, pid=1, conn=NULL, ...) {
   skdf$skid=rep(as.integer(names(res$skeletons)), nrows)
   skdf=merge(skdf, aldf, by='id')
   skdf=skdf[c("skid", "annotation", "id", "uid")]
+}
+
+#' @export
+#' @param annotations Character vector of one or more named annotations to add 
+#'   to the specified neurons.
+#' @details Note that annotations will be created on the catmaid server if they 
+#'   do not already exist (and will not be re-created if they do already exist.)
+#' @examples
+#' \dontrun{
+#' catmaid_set_annotations_for_skeletons(skids=c(10418394,4453485), 'myselection')
+#' catmaid_set_annotations_for_skeletons(skids="annotation:ORN PNs$", 'my pns')
+#' }
+#' @rdname catmaid_get_annotations_for_skeletons
+catmaid_set_annotations_for_skeletons<-function(skids, annotations, pid=1, 
+                                                conn=NULL, ...) {
+  skids=catmaid_skids(skids, conn = conn)
+  post_data=list()
+  post_data[sprintf("skeleton_ids[%d]", seq_along(skids))]=as.list(skids)
+  post_data[sprintf("annotations[%d]", seq_along(annotations))]=as.list(annotations)
+  path=sprintf("/%d/annotations/add", pid)
+  res=catmaid_fetch(path, body=post_data, include_headers = F, 
+                    simplifyVector = T, ...)
+  invisible(res)
 }
