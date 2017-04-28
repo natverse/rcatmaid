283a2438c79f9a2031c9e3ed4706e411149300c2
Gregory Jefferis <jefferis@gmail.com>
2017-04-28 17:48:20 +0100

    tests for summary.catmaidneuron

--- tests/testthat/test-nat.R
+++ tests/testthat/test-nat.R
@@ -10,3 +10,14 @@ test_that("copy fields", {
   
   expect_is(r2 <- copy_tags_connectors(new = r, old = lhn), 'catmaidneuron')
 })
+
+test_that("summary.catmaidneuron behaves", {
+  lhn = readRDS('testdata/lhn.rds')
+  lhn2=lhn
+  class(lhn2)='neuron'
+  expect_is(s1 <- summary(lhn), 'data.frame')
+  expect_is(s2 <- summary(lhn2), 'data.frame')
+  # check the initial summary columns are identical
+  expect_equal(s1[seq_along(s2)], s2)
+  expect_equal(summary(neuronlist(lhn)), s1) 
+})
