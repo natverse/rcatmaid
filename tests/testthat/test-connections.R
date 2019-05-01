2e53885c7ea6a66a04af22dbdf7c28ff6fbc789a
Sridhar Jagannathan <j.sridharrajan@gmail.com>
2019-05-01 15:06:57 +0100

    Updated after Greg's comments for issue #110. See also Develop #112 for detailed review comments. Here I have reverted to the master branch and added test cases for only the environment variables.

--- tests/testthat/test-connections.R
+++ tests/testthat/test-connections.R
@@ -1,6 +1,20 @@
 context("catmaid login and get/post")
 
+#Initial settings for all tests (that can be changed later)
+publicserver <- 'http://hildebrand16.neurodata.io/catmaid/' #public server running a catmaid instance..
+
 test_that("set and get the environmental variables responsible for connections", {
+  
+  #first check if any environment variable has the name of format catmaid. or catmaid_
+  catmaid_msg <- try(catmaid_envstr())
+  if (class(catmaid_msg)[[1]] == 'simpleMessage'){
+    expect_message(message(catmaid_msg), regexp = "catmaid message: No usable environmental variables found") 
+    skip('No environmental variables found so skipping..')
+  } else if (class(catmaid_msg) == "try-error"){
+    #some environmental variables are missing here, so best to skip the tests again.. 
+    skip('Some environmental variables are missing so skipping..')
+  }
+  
   #check the named object has all necessary fields..
   expect_named(catmaid_connection_getenv(), c("server", "authname", "authpassword","token"))
   #check if it returns a specific authname..
@@ -9,16 +23,16 @@ test_that("set and get the environmental variables responsible for connections",
   
   
   #Set environmental variables through connection object and check them back again..
-  conn <- catmaid_connection(server='http://somewhere.org', authname = "flyeee")
+  conn <- catmaid_connection(server=publicserver, authname = "flyeee")
   catmaid_connection_setenv(conn = conn)
-  expect_match(catmaid_connection_getenv()['server'],'http://somewhere.org')
+  expect_match(catmaid_connection_getenv()['server'],publicserver)
   expect_match(catmaid_connection_getenv()['authname'],'flyeee')
   
   #just unset them so the other test cases can run..
   conn['authname'] <- "fly"
   catmaid_connection_setenv(conn = conn)
   expect_match(catmaid_connection_getenv()['authname'],'fly')
-
+  
   
 })
 
@@ -49,9 +63,6 @@ test_that("can make a connection from list elements", {
 })
 
 test_that("can login", {
-  #Just restart the connection here to a public server..
-  conn <- catmaid_login(server='http://hildebrand16.neurodata.io/catmaid/', Cache = FALSE)
-  
   if(inherits(conn, 'try-error')) skip('No catmaid connection')
   expect_is(conn, 'catmaid_connection')
   expect_is(conn$authresponse, 'response')
@@ -59,22 +70,14 @@ test_that("can login", {
 })
 
 test_that("can get and post data", {
-  
-  #Just restart the connection here to a public server..
-  conn <- catmaid_login(server='http://hildebrand16.neurodata.io/catmaid/', Cache = FALSE)
   if(inherits(conn, 'try-error')) skip('No catmaid connection')
- 
-  #Fetch the skeletons first..
-  expect_is(skel<-catmaid_fetch("2/skeletons", conn=conn, parse.json = FALSE),
+  expect_is(skel<-catmaid_fetch("1/10418394/0/0/compact-skeleton", conn=conn, parse.json = FALSE),
             'response')
-  
-  #Get the neuronnames from some skids..
-  skel_parsed<-catmaid_fetch("2/skeletons", conn=conn, parse.json = TRUE)
-  expect_is(neuronnames<-catmaid_fetch("/2/skeleton/neuronnames", conn=conn,
-                                       body=list(pid=2, 'skids[1]'=skel_parsed[[1]], 'skids[2]'=skel_parsed[[7]])),
+  expect_is(neuronnames<-catmaid_fetch("/1/skeleton/neuronnames", conn=conn,
+                                       body=list(pid=1, 'skids[1]'=10418394, 'skids[2]'=4453485)),
             'list')
   # nb we can't rely on the returned order
-  expect_equal(sort(names(neuronnames)), c(as.character(skel_parsed[[1]]),as.character(skel_parsed[[7]])))
+  expect_equal(sort(names(neuronnames)), c('10418394','4453485'))
   expect_equal(names(attributes(neuronnames)), c("names", "url", "headers"))
 })
 
