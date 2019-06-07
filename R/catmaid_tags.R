b958050ad2383810871bb767248f9ca4d3db616a
alexanderbates <alexander.shakeel.bates@gmail.com>
2019-06-07 21:30:34 +0100

    Added catmaid_get_tags
    * Find the location of tags in a CATMAID neuron, either as URLs to the location of a TODO tag in CATMAID or as a data.frame reporting the location and skeleton treenode locations of specified tags

--- R/catmaid_tags.R
+++ R/catmaid_tags.R
@@ -9,8 +9,6 @@
 #' @rdname prune_by_tag
 #' @export
 prune_by_tag <-function(x, ...) UseMethod("prune_by_tag")
-#' @rdname prune_by_tag
-#' @export
 prune_by_tag.neuron <- function(x, tag, remove.upstream = TRUE, ...){
   classes = class(x)
   p = unlist(x$tags[names(x$tags)%in%tag])
@@ -25,8 +23,6 @@ prune_by_tag.neuron <- function(x, tag, remove.upstream = TRUE, ...){
   class(x) = classes
   x
 }
-#' @rdname prune_by_tag
-#' @export
 prune_by_tag.catmaidneuron<- function(x, tag, remove.upstream = TRUE, ...){
   p = unlist(x$tags[names(x$tags)%in%tag])
   if(is.null(p)){
@@ -45,8 +41,6 @@ prune_by_tag.catmaidneuron<- function(x, tag, remove.upstream = TRUE, ...){
   class(y) = c("neuron","catmaidneuron")
   y
 }
-#' @rdname prune_by_tag
-#' @export
 prune_by_tag.neuronlist <- function(x, tag, remove.upstream = TRUE, ...){
   nlapply(x, tag = tag, prune_by_tag, remove.upstream = remove.upstream)
 }
@@ -64,16 +58,12 @@ prune_by_tag.neuronlist <- function(x, tag, remove.upstream = TRUE, ...){
 #' @export
 soma<-function(x, ...) UseMethod("soma")
 
-#' @export
-#' @rdname soma
 soma.neuronlist<-function(x, ...) {
   rdf=plyr::ldply(x, soma, ...)
   rownames(rdf)=rdf[[1]]
   rdf[-1]
 }
 
-#' @export
-#' @rdname soma
 soma.neuron<-function(x) {
   r=if(length(somaid<-x$tags$soma[[1]])){
     x$d[match(somaid, x$d$PointNo),c("X","Y","Z")]
@@ -87,32 +77,68 @@ soma.neuron<-function(x) {
 #' @rdname soma
 somaid<-function(x, ...) UseMethod("somaid")
 
-#' @export
-#' @rdname soma
 somaid.neuronlist<-function(x, ...) {
   unlist(nlapply(x, somaid.neuron, ...))
 }
 
-#' @export
-#' @rdname soma
+
 somaid.neuron <- function(x) unlist(x$tags$soma)
 
 #' @export
 #' @rdname soma
 somapos<-function(x, ...) UseMethod("somaid")
 
-#' @export
-#' @rdname soma
 somapos.neuronlist<-function(x, ...) {
   unlist(nlapply(x, somapos), ...)
 }
 
-#' @export
-#' @rdname soma
 somapos.neuron <- function(x) match(somaid(x), x$d$PointNo)
 
 
+#' Find the location of specified tags for a CATMAID neuron
+#'
+#' @description  Find the location of tags in a CATMAID neuron, either as URLs to the location of a TODO tag in CATMAID or as a data.frame reporting the location and skeleton treenode locations of specified tags.
+#' @param x a neuron or neuronlist object
+#' @param tag a single character specifying which tag to look for. Defaults to TODO
+#' @param only.leaves whether or not to only return leaf nodes with the specified tag
+#' @param url if TRUE (default) a list of URLs pertaining to specified tag locations are returned. If FALSE, a data.frame subsetted from x$d is returned, reporting treenode ID and X,Y,Z positions for specified tags
+#' @param pid project id. Defaults to 1. For making the URL.
+#' @param conn CATMAID connection object, see ?catmaid::catmaid_login for details. For making the URL.
+#' @export
+#' @rdname catmaid_get_tag
+catmaid_get_tag<-function(x, tag = "TODO", url = FALSE, only.leaves = TRUE, conn = NULL, pid = 1) UseMethod("catmaid_get_tag")
 
+catmaid_get_tag.neuron <- function(x, tag = "TODO", url = FALSE, only.leaves = TRUE, conn = NULL, pid = 1){
+  TODO = unique(unlist(x$tags[[tag]]))
+  if(only.leaves){
+    TODO = TODO[TODO%in%x$d$PointNo[nat::endpoints(x)]]
+  }
+  if(is.null(TODO)){
+    NULL
+  }else if(length(TODO)){
+    df = subset(x$d,PointNo%in%TODO)
+    if(url){
+      catmaid_url = paste0(catmaid_get_server(conn), "?pid=",pid)
+      catmaid_url = paste0(catmaid_url, "&zp=", df[["Z"]])
+      catmaid_url = paste0(catmaid_url, "&yp=", df[["Y"]])
+      catmaid_url = paste0(catmaid_url, "&xp=", df[["X"]])
+      catmaid_url = paste0(catmaid_url, "&tool=tracingtool")
+      catmaid_url = paste0(catmaid_url, "&sid0=5&s0=0")
+      invisible(catmaid_url)
+    }
+    else{
+      df
+    }
+  }
+}
+
+catmaid_get_tag.neuronlist <- function(x, tag = "TODO", url = FALSE, only.leaves = TRUE, conn = NULL, pid = 1){
+  if(url){
+    unlist(lapply(x,catmaid_get_tag.neuron, url=url, tag= tag))
+  }else{
+    do.call(rbind,lapply(x,catmaid_get_tag.neuron, url=url, tag = tag))
+  }
+}
 
 
 
