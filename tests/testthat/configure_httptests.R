8de36c964b1f98d86d0fe355c807763ff721950f
Sridhar Jagannathan <j.sridharrajan@gmail.com>
2019-06-11 15:54:45 +0100

    Updated for adding testcases for test-annotation

---
+++ tests/testthat/configure_httptests.R
@@ -0,0 +1,38 @@
+#This file contains details on how to cache http requests before using them in mock apis for offline testing
+library(httptest)
+library(catmaid)
+
+#Step 1: Set the folder locations..
+testpath = paste0('./',test_path())
+.mockPaths(testpath)
+
+#Step 2: Start capturing http data from now..
+
+start_capturing()
+
+#Step 3: Connect to a valid catmaid server..
+conn <- catmaid_login()
+
+#Now add test cases for each file..
+
+#For test file : test-annotation.R
+      #For test case : "catmaid_get_annotations_for_skeletons"
+tempval <- catmaid_skids('annotation:^ORN$')
+skid_1 <- tempval[[1]]
+skid_2 <- tempval[[2]]
+pid <- 1
+
+n1 <- catmaid_get_annotations_for_skeletons(skids = skid_1)
+n2 <- catmaid_get_annotations_for_skeletons(skids = skid_2)
+n12 <- catmaid_get_annotations_for_skeletons(skids = c(skid_1, skid_2))
+
+pathvar = paste0(pid,"/",skid_1,"/0/0/compact-skeleton")
+skel<-catmaid_fetch(pathvar, conn=conn, parse.json = FALSE)
+neuronnames<-catmaid_fetch("/1/skeleton/neuronnames", conn=conn,body=list(pid=pid, 
+                          'skids[1]'=skid_1, 'skids[2]'=skid_2))
+
+#For test file : 
+      #For test case :
+
+stop_capturing()
+
--- tests/testthat/test-annotation.R
+++ tests/testthat/test-annotation.R
@@ -1,13 +1,35 @@
 context("catmaid annotation queries")
 
-# nb will reuse cached connection made earlier
-conn=try(catmaid_login(), silent = TRUE)
+library(catmaid)
+library(httptest)
 
-test_that("catmaid_get_annotations_for_skeletons", {
-  if(inherits(conn, 'try-error')) skip('No catmaid connection')
-  
-  expect_is(n1<-catmaid_get_annotations_for_skeletons(skids=c(10418394)), 'data.frame')
-  n2=catmaid_get_annotations_for_skeletons(skids=c(4453485))
-  expect_is(n12 <- catmaid_get_annotations_for_skeletons(skids=c(10418394,4453485)), 'data.frame')
-  expect_equal(rbind(n1,n2), n12)
-})
+fakeconn <-
+  structure(
+    list(
+      server = "https://neuropil.janelia.org/tracing/fafb/v14",
+      nologin = TRUE,
+      authresponse = TRUE,
+      config=httr::config()
+    ),
+    .class = "catmaid_connection"
+  )
+
+conn = fakeconn
+
+test_that("Check connection",{
+            expect_equal(fakeconn$server,"https://neuropil.janelia.org/tracing/fafb/v14") 
+ })
+ 
+with_mock_api(tempval <- catmaid_skids('annotation:^ORN$'))
+skid_1 <-tempval[[1]]
+skid_2 <-tempval[[2]]
+
+with_mock_api(
+  test_that("catmaid_get_annotations_for_skeletons", {
+    if (inherits(conn, 'try-error')) skip('No catmaid connection')
+    expect_is(n1 <- catmaid_get_annotations_for_skeletons(skids = skid_1),'data.frame')
+    n2 = catmaid_get_annotations_for_skeletons(skids = skid_2)
+    expect_is(n12 <- catmaid_get_annotations_for_skeletons(skids = c(skid_1, skid_2)),'data.frame')
+    expect_equal(rbind(n1, n2), n12)
+ 
+}))
