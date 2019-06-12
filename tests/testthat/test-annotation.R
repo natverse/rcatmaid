743d942438d6e24f45f967734a027902f5ac81e7
Sridhar Jagannathan <j.sridharrajan@gmail.com>
2019-06-12 14:36:16 +0100

    Added support for public live server

--- tests/testthat/test-annotation.R
+++ tests/testthat/test-annotation.R
@@ -3,27 +3,45 @@ context("catmaid annotation queries")
 library(catmaid)
 library(httptest)
 
-fakeconn <-
-  structure(
-    list(
-      server = "https://neuropil.janelia.org/tracing/fafb/v14",
-      nologin = TRUE,
-      authresponse = TRUE,
-      config=httr::config()
-    ),
-    .class = "catmaid_connection"
-  )
-
-conn = fakeconn
+##Set up the configuration server for mocks..
+privateserver <- "https://neuropil.janelia.org/tracing/fafb/v14"
+fakeconn <- structure( list( server = privateserver,
+                             nologin = TRUE, authresponse = TRUE, config=httr::config() ),
+                      .class = "catmaid_connection")
 
 set_requester(function (request) {
   gsub_request(request, "https://neuropil.janelia.org/tracing/fafb/v14/", "api/")
 })
 
-test_that("Check connection",{
-            expect_equal(fakeconn$server,"https://neuropil.janelia.org/tracing/fafb/v14") 
+
+##Set up the configuration server now for live public configurations..
+publicserver <- "https://l1em.catmaid.virtualflybrain.org"
+pubconn <- catmaid_connection(server=publicserver, authname = "flyeee")
+
+
+#Set the environmental variable responsible for mocking
+Sys.setenv(MOCK_BYPASS = "true") #"true" (public live server) , "capture" (mock server) 
+
+##Now check if the test is with real server or mock server
+if (Sys.getenv("MOCK_BYPASS") == "true") {
+    with_mock_api <- force
+    conn <- pubconn
+    print(paste0("Testing with public live server: ",publicserver, sep=""))
+} else if (Sys.getenv("MOCK_BYPASS") == "capture") {
+    conn <- fakeconn
+    print("Testing with mock server")
+}
+
+
+test_that("Check mock connection",{
+            expect_equal(fakeconn$server,privateserver) 
  })
+
+test_that("Check live connection",{
+  expect_equal(pubconn$server,publicserver) 
+})
  
+
 with_mock_api(tempval <- catmaid_skids('annotation:^ORN$',conn = conn))
 skid_1 <-tempval[[1]]
 skid_2 <-tempval[[2]]
