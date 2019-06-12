a077d9b13c2a5adbc2e9c347d52aa4fd604c957a
Sridhar Jagannathan <j.sridharrajan@gmail.com>
2019-06-12 15:03:25 +0100

    Updated tests so both public and mock server connections happen at same time.

--- tests/testthat/test-annotation.R
+++ tests/testthat/test-annotation.R
@@ -18,20 +18,11 @@ set_requester(function (request) {
 publicserver <- "https://l1em.catmaid.virtualflybrain.org"
 pubconn <- catmaid_login(server=publicserver, Cache = FALSE)
 
+print(paste0("Testing with public live server: ",publicserver, sep=""))
+print(paste0("Testing with mock server: ",privateserver, sep=""))
 
 #Set the environmental variable responsible for mocking
 Sys.setenv(MOCK_BYPASS = "true") #"true" (public live server) , "capture" (mock server) 
-
-##Now check if the test is with real server or mock server
-if (Sys.getenv("MOCK_BYPASS") == "true") {
-    with_mock_api <- force
-    conn <- pubconn
-    print(paste0("Testing with public live server: ",publicserver, sep=""))
-} else if (Sys.getenv("MOCK_BYPASS") == "capture") {
-    conn <- fakeconn
-    print("Testing with mock server")
-}
-
 
 test_that("Check mock connection",{
             expect_equal(fakeconn$server,privateserver) 
@@ -41,17 +32,34 @@ test_that("Check live connection",{
   expect_equal(pubconn$server,publicserver) 
 })
  
-
-with_mock_api(tempval <- catmaid_skids('annotation:^ORN$',conn = conn))
+##Tests with mock connection first..
+context("catmaid annotation queries-mock connection")
+with_mock_api(tempval <- catmaid_skids('annotation:^ORN$',conn = fakeconn))
 skid_1 <-tempval[[1]]
 skid_2 <-tempval[[2]]
 
 with_mock_api(
   test_that("catmaid_get_annotations_for_skeletons", {
-    if (inherits(conn, 'try-error')) skip('No catmaid connection')
-    expect_is(n1 <- catmaid_get_annotations_for_skeletons(skids = skid_1,conn = conn),'data.frame')
-    n2 = catmaid_get_annotations_for_skeletons(skids = skid_2,conn = conn)
-    expect_is(n12 <- catmaid_get_annotations_for_skeletons(skids = c(skid_1, skid_2),conn = conn),'data.frame')
+    if (inherits(fakeconn, 'try-error')) skip('No catmaid connection')
+    expect_is(n1 <- catmaid_get_annotations_for_skeletons(skids = skid_1,conn = fakeconn),'data.frame')
+    n2 = catmaid_get_annotations_for_skeletons(skids = skid_2,conn = fakeconn)
+    expect_is(n12 <- catmaid_get_annotations_for_skeletons(skids = c(skid_1, skid_2),conn = fakeconn),'data.frame')
     expect_equal(rbind(n1, n2), n12)
  
 }))
+
+
+##Tests with public live connection next..
+context("catmaid annotation queries-public connection")
+tempval <- catmaid_skids('annotation:^ORN$',conn = pubconn)
+skid_1 <-tempval[[1]]
+skid_2 <-tempval[[2]]
+
+test_that("catmaid_get_annotations_for_skeletons", {
+    if (inherits(pubconn, 'try-error')) skip('No catmaid connection')
+    expect_is(n1 <- catmaid_get_annotations_for_skeletons(skids = skid_1,conn = pubconn),'data.frame')
+    n2 = catmaid_get_annotations_for_skeletons(skids = skid_2,conn = pubconn)
+    expect_is(n12 <- catmaid_get_annotations_for_skeletons(skids = c(skid_1, skid_2),conn = pubconn),'data.frame')
+    expect_equal(rbind(n1, n2), n12)
+    
+  })
